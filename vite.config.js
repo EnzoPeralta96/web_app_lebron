61dc672a72748607d488142b927bf2ca4cf8a83e
metzgermeist <javiermas.unt@gmail.com>
2025-11-11 23:57:33 -0300

    Rebuild automático: nueva versión lista para Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/web_app_lebron/',
+  build: {
+    outDir: 'docs', // 👉 hace que el build se genere en la carpeta docs
+  },
 })
